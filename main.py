8a7891d5ea3094706b0e172e2addc229f1e4a595
DariaMinina <daria.minina1@outlook.com>
2022-08-02 20:57:20 +0300

    added config.yaml and settings for work with config

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from aiohttp import web
 import jinja2
 import aiohttp_jinja2
 
+from app.settings import config, BASE_DIR
+
+
+def setup_config(app: web.Application):
+    app["config"] = config
+
 
 def setup_routes(app: web.Application) -> None:
     from app.forum.routes import setup_routes as setup_forum_routes
@@ -9,10 +15,14 @@ def setup_routes(app: web.Application) -> None:
 
 
 def setup_external_libraries(app: web.Application) -> None:
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("templates"))
+    aiohttp_jinja2.setup(
+        app,
+        loader=jinja2.FileSystemLoader(f"{BASE_DIR}/templates")
+    )
 
 
 def setup_app(app: web.Application) -> None:
+    setup_config(app)
     setup_external_libraries(app)
     setup_routes(app)
 
@@ -22,5 +32,6 @@ app = web.Application()
 
 if __name__ == "__main__":
     setup_app(app)
-    web.run_app(app)
+    web.run_app(app, port=config["common"]["port"])
+
 
